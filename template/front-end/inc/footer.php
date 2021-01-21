d47fd6004abc308dee32eeb9e16a7098e5204130
MadHowl <madhowl@yandex.ru>
2021-01-21 10:55:37 +1000

    add templates

---
+++ template/front-end/inc/footer.php
@@ -0,0 +1,99 @@
+</div><!-- End blog entries list -->
+
+<div class="col-lg-4">
+
+    <div class="sidebar">
+
+        <h3 class="sidebar-title">Search</h3>
+        <div class="sidebar-item search-form">
+            <form action="">
+                <input type="text">
+                <button type="submit"><i class="icofont-search"></i></button>
+            </form>
+        </div><!-- End sidebar search formn-->
+
+        <h3 class="sidebar-title">Categories</h3>
+        <div class="sidebar-item categories">
+            <ul>
+                <li><a href="#">General <span>(25)</span></a></li>
+                <li><a href="#">Lifestyle <span>(12)</span></a></li>
+                <li><a href="#">Travel <span>(5)</span></a></li>
+                <li><a href="#">Design <span>(22)</span></a></li>
+                <li><a href="#">Creative <span>(8)</span></a></li>
+                <li><a href="#">Educaion <span>(14)</span></a></li>
+            </ul>
+        </div><!-- End sidebar categories-->
+
+
+    </div><!-- End sidebar -->
+
+</div><!-- End blog sidebar -->
+
+</div>
+
+</div>
+</section><!-- End Blog Section -->
+
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+
+
+                <div class="col-lg-12 col-md-12 footer-newsletter">
+                    <h4>{{ contacts }}</h4>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+        <div class="mr-md-auto text-center text-md-left">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Presento</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/presento-bootstrap-corporate-template/ -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+        <div class="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="/template/front-end/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/template/front-end/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/template/front-end/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="/template/front-end/assets/vendor/php-email-form/validate.js"></script>
+<script src="/template/front-end/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="/template/front-end/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+<script src="/template/front-end/assets/vendor/counterup/counterup.min.js"></script>
+<script src="/template/front-end/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/template/front-end/assets/vendor/venobox/venobox.min.js"></script>
+<script src="/template/front-end/assets/vendor/aos/aos.js"></script>
+
+<!-- Template Main JS File -->
+<script src="/template/front-end/assets/js/main.js"></script>
+
+</body>
+
